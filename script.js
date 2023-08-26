91a41ee4e5c55ff96be82023dd939e321651cabc
ahmadsuryapamungkas <suryajrahmad@gmail.com>
2023-08-26 13:56:01 +0700

    merapikan javascript

--- script.js
+++ script.js
@@ -5,9 +5,6 @@ let reactions = [
   { equation: 'Massa molar karbon dioksida (CO2) adalah sekitar 44 g/mol. Berapa massa dari 1,5 mol karbon dioksida ?', answer: 66 },
   { equation: ' Massa molar air (H2O) adalah sekitar 18 g/mol. Berapa massa dari 5 mol air ?', answer: 90 },
   { equation: ' Massa molar magnesium sulfat (MgSO4) adalah sekitar 120 g/mol. Berapa massa dari 0,3 mol magnesium sulfat ?', answer: 36 },
-
-  // Tambahkan pe: 2 mrtanyaan dan jawaban lainnya di sini
-
 ];
 
 let currentReactionIndex;
@@ -15,29 +12,30 @@ let currentReaction;
 let score = 0;
 
 function getRandomReaction() {
-  if (reactions.length > 0 && typeof reactions !== 'undefined' ) { // ketika masih ada soal
+  if (reactions.length > 0 && typeof reactions !== 'undefined') {
+    // ketika masih ada soal
     currentReactionIndex = Math.floor(Math.random() * reactions.length);
     currentReaction = reactions[currentReactionIndex];
     document.getElementById('questionText').textContent = currentReaction.equation;
-    
+
     // hapus soal yang telah dijawab benar agar tidak dimunculkan lagi
-    reactions.splice(currentReactionIndex, 1);  
-  } else { // ketika soalnya sudah habis
-    console.log("heree");
-    document.getElementById('questionText').textContent = "Selamat Anda telah menyelesaikan semua";
-    document.getElementById('questionText').style.color = 'green'
-    document.getElementById('questionText').style.fontSize = '24px'
+    reactions.splice(currentReactionIndex, 1);
+  } else {
+    // ketika soalnya sudah habis
+    console.log('heree');
+    document.getElementById('questionText').textContent = 'Selamat Anda telah menyelesaikan semua';
+    document.getElementById('questionText').style.color = 'green';
+    document.getElementById('questionText').style.fontSize = '24px';
   }
 }
 
 function checkAnswer() {
   const userAnswer = parseFloat(document.getElementById('userAnswer').value);
   const correctAnswer = currentReaction.answer;
-  
+
   if (userAnswer === correctAnswer) {
     document.getElementById('result').textContent = 'Selamat! Jawaban benar.';
     document.getElementById('result').style.color = 'green';
-    
 
     // menambahkan score
     score++;
@@ -47,7 +45,7 @@ function checkAnswer() {
   }
 
   document.getElementById('userAnswer').value = '';
-  document.getElementById('score').textContent = `Score: ${score}`
+  document.getElementById('score').textContent = `Score: ${score}`;
   getRandomReaction();
 }
 
